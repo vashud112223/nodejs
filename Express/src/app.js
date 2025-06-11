0ef4d20989e9f2526e651cd4753aed8f03750868
vashud112223 <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-06-11 23:59:11 +0530

    exploring http methods

--- Express/src/app.js
+++ Express/src/app.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use('/hello',(req,res)=> {
-    res.end("Hello from the server")
+// app.use('/',(req,res)=> {
+//     res.end("namaste")
+// })
+
+// after the / whatever path we will give it still give the same output because it starts with /
+// order matters here
+// app.use('/hello',(req,res)=> {
+//     res.end("Hello from the server")
+// })
+// if we use (use) it will matches all the http methods
+
+app.get('/user',(req,res)=>{
+    console.log(req.query)
+    res.send({"firstname":"Ashu","lastname":"verma"})
+})
+
+app.get('/user/:userId/:name/:password',(req,res)=>{
+    console.log(req.params)
+    res.send({"firstname":"Ashu","lastname":"verma"})
+})
+// we can also give path as (ab?c),(ab+c),a(bc)+d,/a/
+
+app.post('/user',(req,res)=>{
+    res.send("User added succesfully")
 })
 app.listen(3000,()=> {
     console.log("Server is successfully on port 3000")
