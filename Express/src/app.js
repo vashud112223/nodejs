2bc3649ed365518dd955b7b4145d93fca323fd94
vashud112223 <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-06-12 00:13:33 +0530

    multiple route handlers

--- Express/src/app.js
+++ Express/src/app.js
@@ -13,6 +13,20 @@ const app = express();
 // })
 // if we use (use) it will matches all the http methods
 
+// app.use('/route',rh,[rh2,rh3],rh4) rh-route handler
+app.use('/user',[(req,res,next)=>{
+    console.log("Handling the route user ")
+      next();
+//    res.send(" Response")
+ 
+    // res.send({"firstname":"Ashu","lastname":"verma"}) // if we are not send the response back it will hanging
+    //there are multiple route handler
+},(req,res)=>{
+   console.log("Handling the route user 2")
+   res.send("2nd Response")
+
+}])
+
 app.get('/user',(req,res)=>{
     console.log(req.query)
     res.send({"firstname":"Ashu","lastname":"verma"})
@@ -27,6 +41,9 @@ app.get('/user/:userId/:name/:password',(req,res)=>{
 app.post('/user',(req,res)=>{
     res.send("User added succesfully")
 })
+
+
+
 app.listen(3000,()=> {
     console.log("Server is successfully on port 3000")
 });
