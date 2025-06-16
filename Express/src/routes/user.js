fccf0ffc84e0378e51458ee6f00aac13c5bd5352
vashud112223 <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-06-16 23:18:37 +0530

    feed api and pagination

--- Express/src/routes/user.js
+++ Express/src/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
+const User = require("../models/user");
+
+const USER_SAFE_DATA = "fromUserID firstName lastName age gender skills";
 
 userRouter.get("/users/request/received", userAuth, async (req, res) => {
   try {
@@ -47,6 +50,51 @@ userRouter.get("/users/connections", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(404).send("Error: " + err.message);
+  }
+});
+
+//user should see all the user cards except
+// his own card
+// his connections
+// ignored people
+// already sent the connection request
+
+userRouter.get("/users/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInid = req.user._id;
+    const page = parseInt(req.query?.page) || 1;
+    let limit = parseInt(req.query?.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    // find connectionRequests
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserID: loggedInid }, { toUserID: loggedInid }],
+    }).select("fromUserID toUserID");
+    // write logic to hide the connection user
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserID);
+      hideUsersFromFeed.add(req.toUserID);
+    });
+
+    // finally find the feed
+
+    const users = await User.find({
+      $and: [
+        {
+          _id: { $nin: Array.from(hideUsersFromFeed) },
+        },
+        { _id: { $ne: loggedInid } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
   }
 });
 
