a0b56dd7af5a1bd7132a41e0b53a1416e7bedc39
vashud112223 <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-06-23 19:59:44 +0530

    implement the logic for sending email using aws ses

--- Express/src/utils/sesClient.js
+++ Express/src/utils/sesClient.js
@@ -1,8 +1,6 @@
 const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
 const REGION = "us-east-1";
-<<<<<<< HEAD
-
 // Create SES service object.
   console.log({"process":process.env.AWS_ACCESS_KEY})
 const sesClient = new SESClient({
@@ -12,15 +10,4 @@ const sesClient = new SESClient({
     secretAccessKey: process.env.AWS_SECRET_KEY,
   },
 });
-
-=======
-// Create SES service object.
-const sesClient = new SESClient({
-  region: REGION,
-  credentials: {
-    accessKeyId: "AKIA5LRAGZNVBFT6A2PJ",
-    secretAccessKey: "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",
-  },
-});
->>>>>>> c7bb3f4 (implement the logic for sending email using aws ses)
 module.exports = { sesClient };
